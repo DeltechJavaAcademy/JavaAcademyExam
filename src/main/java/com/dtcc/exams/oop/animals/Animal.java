942778447c66918e251795d9e226f5b3c01624f3
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-10-09 01:24:04 -0400

    k

--- src/main/java/com/dtcc/exams/oop/animals/Animal.java
+++ src/main/java/com/dtcc/exams/oop/animals/Animal.java
@@ -7,7 +7,15 @@ import com.dtcc.exams.oop.Person;
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Person` class before attempting this class.
  */
 public class Animal {
+    private long id;
+    private Person person;
+    private Address address;
+
     public Animal() {
+        this.id = id;
+        this.person = person;
+        this.address = address;
+
     }
 
     /**
@@ -15,23 +23,28 @@ public class Animal {
      * @param owner - owner of animal
      */
     public Animal(Long id, Person owner) {
+        this.id = id;
+        this.person = owner;
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Person getOwner() {
-        return null;
+        return this.person;
     }
 
     public void setOwner(Person owner) {
+        this.person = owner;
     }
 
     public Address getAddress() {
-        return null;
+        return this.address;
     }
 }
+
